bc3104d53145c085a34d4a3e3f43ee7d4ed6154d
MonikaKub <113998101+MonikaKub@users.noreply.github.com>
2022-09-27 16:25:23 +0200

    Update ukol.cpp

--- ukol.cpp
+++ ukol.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main() {
@@ -7,13 +8,33 @@ int main() {
     std::string jmeno;
     std::cin >>jmeno;
 
-    std::cout << "Jmeno je: " << jmeno << "n\"
+    std::cout << "Jmeno je: " << jmeno << "\n";
 
-    enum vzdelani {zs, ss, vs};
+    std::cout <<"Zadej prijmeni: ";
+    std::string prijmeni;
+    std::cin >>prijmeni;
 
-    vzdelani skola = ss;
+    std::cout << "Prijmeni je: " << prijmeni << "\n";
+int vzdelani;
+    std::cout <<"Zadej vzdelani (zs = 1, ss = 2, vs == 3): ";
 
-    std::cout << "Vzdelani je " << skola << "n\";
+
+    //enum vzdelani {zs, ss, vs};
+
+    //vzdelani skola = vs;
+
+    if (vzdelani == 1){
+    std::cout << "Vzdelani je zakladni skola \n";
+    }
+    else if (vzdelani == 2){
+    std::cout << "Vzdelani je stredni skola \n";
+    }
+    else if (vzdelani == 3){
+    std::cout << "Vzdelani je vysoka skola \n";
+    }
+
+    std::ofstream vystup("vystup.txt"); // ulozi do souboru
+    vystup << "Jmeno: " << jmeno << "\n";
 
     return 0;
 }
