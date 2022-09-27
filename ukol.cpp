a9ba0f2fa73f040bb96e4ceaa1002aef15227c89
MonikaKub <113998101+MonikaKub@users.noreply.github.com>
2022-09-27 16:20:03 +0200

    Add files via upload

---
+++ ukol.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::cout <<"Zadej jmeno: ";
+    std::string jmeno;
+    std::cin >>jmeno;
+
+    std::cout << "Jmeno je: " << jmeno << "n\"
+
+    enum vzdelani {zs, ss, vs};
+
+    vzdelani skola = ss;
+
+    std::cout << "Vzdelani je " << skola << "n\";
+
+    return 0;
+}
+
